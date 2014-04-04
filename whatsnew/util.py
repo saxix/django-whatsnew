8cb7f8397862c0b8b7e4dc7e0870bc2f961c228d
sax <s.apostolico@gmail.com>
2014-04-04 21:15:07 +0200

    bug fixes

--- whatsnew/util.py
+++ whatsnew/util.py
@@ -1,21 +1,4 @@
-from types import StringTypes
-import datetime
 import pkg_resources
-from whatsnew.models import WhatsNew
-from django.db.models import Q
-from .fields import Version
-
-def display_message(request, package_name, cookie_name):
-    today = datetime.datetime.today()
-
-    last_seen = Version(request.COOKIES.get(cookie_name) or '0.0')
-    current_version = Version(get_version(package_name))
-    try:
-        last = WhatsNew.objects.filter(enabled=True).filter(Q(expire__gte=today) | Q(expire=None)).latest()
-        display = last.version.same(current_version) and last.version != last_seen
-    except WhatsNew.DoesNotExist:
-        display = False
-    return display, last
 
 
 def get_version(package_name):
